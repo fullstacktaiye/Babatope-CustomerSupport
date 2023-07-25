24588921414b807b3aa64aad8e4ff4c5c237b362
fullstacktaiye <111784920+fullstacktaiye@users.noreply.github.com>
2023-07-25 12:32:08 -0500

    Upgraded Java version from 11 to 17
    I upgraded the Java version to 17 because Java 11 has dependency issues with SpringMVC 6. I bootstrapped Spring as well in here.

---
+++ src/main/java/com/example/babatopecustomersupport/config/RootContextConfig.java
@@ -0,0 +1,12 @@
+package com.example.babatopecustomersupport.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.babatopecustomersupport.site",
+        excludeFilters = @ComponentScan.Filter(Controller.class))
+public class RootContextConfig {
+}
+
